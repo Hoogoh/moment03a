b5fedf96afdbc679df917aec6add139b24c18377
Hoogoh <98016405+Hoogoh@users.noreply.github.com>
2022-10-27 13:41:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+"""""
+CorrectUsername = 'Hugo'
+CorrectPassword = 'Hugo'
 
-from time import localtime, strftime
-t = localtime()
-print(t)
-print(t.tm_hour)
-print(t.tm_min)
-print(t.tm_sec)
-print(strftime('%Y-%m-%d %H:%M:%S', t))
+username = input('Write your username here: ')
+if username == CorrectUsername:
+    password = input('Write your password here: ')
+    if password == CorrectPassword:
+        print('Access Granted')
+"""""
 
-if t.tm_hour > 16:
-    print("GÅ hem")
+import random
+
+guess_made = 0
+
+name = input('Hello there! What shall I call you?\n')
+
+number = random.randint(0, 10)
+print('Well, {0}, I am thinking of a number between 0 and 10000.'.format(name))
+
+while guess_made < 10:
+
+    guess = int(input('Take a guess: '))
+
+    guess_made += 1
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guess_made))
+else:
+    print('Nope. The number I was thinking of was {0}'.format(number))
